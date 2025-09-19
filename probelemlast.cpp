86542ad3523dbbc0a3a9407ff8fd70a679a62547
Shaxzoda <250139@newuu.uz>
2025-09-19 10:30:02 +0500

    Add C++ solutions for problems 13-24

---
+++ probelemlast.cpp
@@ -0,0 +1,24 @@
+//
+// Created by xoshi on 9/19/2025.
+//
+
+#include "probelemlast.h"
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <bits/locale_classes.h>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int ng = 10 + rand() % 90;
+    int usernumber;
+    cin >> usernumber;
+     int u1=usernumber/100;
+    int u2=usernumber%100;
+    int g1=ng/10;
+    int g2=ng%10;
+    cout << (u1 && u2 && &&) << endl;
+}
